78451f1dbbc14e9e4585875a79c23da229854553
Marcos Juan <mjuan@thehotelsnetwork.com>
2024-04-30 10:18:14 +0200

    wezterm && tmux

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,260 @@
+--
+-- ██╗    ██╗███████╗███████╗████████╗███████╗██████╗ ███╗   ███╗
+-- ██║    ██║██╔════╝╚══███╔╝╚══██╔══╝██╔════╝██╔══██╗████╗ ████║
+-- ██║ █╗ ██║█████╗    ███╔╝    ██║   █████╗  ██████╔╝██╔████╔██║
+-- ██║███╗██║██╔══╝   ███╔╝     ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║
+-- ╚███╔███╔╝███████╗███████╗   ██║   ███████╗██║  ██║██║ ╚═╝ ██║
+--  ╚══╝╚══╝ ╚══════╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝
+-- A GPU-accelerated cross-platform terminal emulator
+-- https://wezfurlong.org/wezterm/
+
+local dark_opacity = 1 -- 0.85
+local light_opacity = 1 -- 0.9
+
+local b = require("utils/background")
+local cs = require("utils/color_scheme")
+local h = require("utils/helpers")
+local k = require("utils/keys")
+local w = require("utils/wallpaper")
+
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+local config = {
+	background = {
+		b.get_background(dark_opacity, light_opacity),
+	},
+
+	font_size = 16,
+
+	line_height = 1.1,
+	font = wezterm.font_with_fallback({
+		"Berkeley Mono",
+		"JetBrainsMono Nerd Font",
+		-- "CommitMono",
+		-- "DengXian",
+	}),
+
+	color_scheme = cs.get_color_scheme(),
+
+	window_padding = {
+		left = 10,
+		right = 10,
+		top = 10,
+		bottom = 10,
+	},
+
+	set_environment_variables = {
+		-- THEME_FLAVOUR = "latte",
+		BAT_THEME = h.is_dark() and "Catppuccin-mocha" or "Catppuccin-latte",
+		TERM = "xterm-256color",
+		LC_ALL = "en_US.UTF-8",
+	},
+
+	-- general options
+	adjust_window_size_when_changing_font_size = false,
+	debug_key_events = false,
+	enable_tab_bar = false,
+	native_macos_fullscreen_mode = false,
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "RESIZE",
+
+	-- keys
+	keys = {
+		-- k.cmd_key(".", k.multiple_actions(":ZenMode")),
+		-- k.cmd_key("[", act.SendKey({ mods = "CTRL", key = "o" })),
+		-- k.cmd_key("]", act.SendKey({ mods = "CTRL", key = "i" })),
+		k.cmd_key("p", k.multiple_actions(":Telescope find_files")),
+		k.cmd_key("f", k.multiple_actions(":Telescope live_grep")),
+		-- k.cmd_key("H", act.SendKey({ mods = "CTRL", key = "h" })),
+		-- k.cmd_key("J", act.SendKey({ mods = "CTRL", key = "j" })),
+		-- k.cmd_key("K", act.SendKey({ mods = "CTRL", key = "k" })),
+		-- k.cmd_key("L", act.SendKey({ mods = "CTRL", key = "l" })),
+		-- k.cmd_key("P", k.multiple_actions(":GoToCommand")),
+		-- k.cmd_key("p", k.multiple_actions(":GoToFile")),
+		-- k.cmd_key("j", k.multiple_actions(":GoToFile")),
+		k.cmd_key("j", act.SendKey({ mods = "CTRL", key = "j" })),
+		k.cmd_key("k", act.SendKey({ mods = "CTRL", key = "k" })),
+		k.cmd_key("l", k.multiple_actions("clear")),
+		k.cmd_key("q", k.multiple_actions(":qa!")),
+		k.cmd_key("w", k.multiple_actions(":bd")),
+		k.cmd_to_tmux_prefix("1", "1"),
+		k.cmd_to_tmux_prefix("2", "2"),
+		k.cmd_to_tmux_prefix("3", "3"),
+		k.cmd_to_tmux_prefix("4", "4"),
+		k.cmd_to_tmux_prefix("5", "5"),
+		k.cmd_to_tmux_prefix("6", "6"),
+		k.cmd_to_tmux_prefix("7", "7"),
+		k.cmd_to_tmux_prefix("8", "8"),
+		k.cmd_to_tmux_prefix("9", "9"),
+		k.cmd_to_tmux_prefix("`", "n"),
+		k.cmd_to_tmux_prefix("b", "B"),
+		k.cmd_to_tmux_prefix("C", "C"),
+		k.cmd_to_tmux_prefix("d", "D"),
+		k.cmd_to_tmux_prefix("G", "G"),
+		k.cmd_to_tmux_prefix("g", "g"),
+		k.cmd_to_tmux_prefix("K", "T"),
+		-- k.cmd_to_tmux_prefix("k", "K"),
+		-- k.cmd_to_tmux_prefix("l", "L"),
+		k.cmd_to_tmux_prefix("n", '"'),
+		k.cmd_to_tmux_prefix("N", "%"),
+		k.cmd_to_tmux_prefix("o", "u"),
+		k.cmd_to_tmux_prefix("T", "!"),
+		k.cmd_to_tmux_prefix("t", "c"),
+		-- k.cmd_to_tmux_prefix("w", "x"),
+		k.cmd_to_tmux_prefix("z", "z"),
+		k.cmd_to_tmux_prefix("Z", "Z"),
+
+		k.cmd_key(
+			"R",
+			act.Multiple({
+				act.SendKey({ key = "\x1b" }), -- escape
+				k.multiple_actions(":source %"),
+			})
+		),
+
+		k.cmd_key(
+			"s",
+			act.Multiple({
+				act.SendKey({ key = "\x1b" }), -- escape
+				k.multiple_actions(":lua SaveAndDoMore()"),
+			})
+		),
+
+		-- {
+		-- 	mods = "CMD|SHIFT",
+		-- 	key = "}",
+		-- 	action = act.Multiple({
+		-- 		act.SendKey({ mods = "CTRL", key = "b" }),
+		-- 		act.SendKey({ key = "n" }),
+		-- 	}),
+		-- },
+		-- {
+		-- 	mods = "CMD|SHIFT",
+		-- 	key = "{",
+		-- 	action = act.Multiple({
+		-- 		act.SendKey({ mods = "CTRL", key = "b" }),
+		-- 		act.SendKey({ key = "p" }),
+		-- 	}),
+		-- },
+
+		{
+			mods = "CTRL",
+			key = "Tab",
+			action = act.Multiple({
+				act.SendKey({ mods = "CTRL", key = "b" }),
+				act.SendKey({ key = "n" }),
+			}),
+		},
+		{
+			mods = "CTRL|SHIFT",
+			key = "Tab",
+			action = act.Multiple({
+				act.SendKey({ mods = "CTRL", key = "b" }),
+				act.SendKey({ key = "n" }),
+			}),
+		},
+
+		{
+			mods = "CTRL",
+			key = "l",
+			action = act.Multiple({
+				act.SendKey({ mods = "CTRL", key = "l" }),
+			}),
+		},
+
+		-- FIX: disable binding
+		-- {
+		-- 	mods = "CMD",
+		-- 	key = "`",
+		-- 	action = act.Multiple({
+		-- 		act.SendKey({ mods = "CTRL", key = "b" }),
+		-- 		act.SendKey({ key = "n" }),
+		-- 	}),
+		-- },
+
+		{
+			mods = "CMD",
+			key = "~",
+			action = act.Multiple({
+				act.SendKey({ mods = "CTRL", key = "b" }),
+				act.SendKey({ key = "p" }),
+			}),
+		},
+	},
+}
+
+wezterm.on("user-var-changed", function(window, pane, name, value)
+	-- local appearance = window:get_appearance()
+	-- local is_dark = appearance:find("Dark")
+	local overrides = window:get_config_overrides() or {}
+	wezterm.log_info("name", name)
+	wezterm.log_info("value", value)
+
+	if name == "T_SESSION" then
+		local session = value
+		wezterm.log_info("is session", session)
+		overrides.background = {
+			w.set_tmux_session_wallpaper(value),
+			{
+				source = {
+					Gradient = {
+						colors = { "#000000" },
+					},
+				},
+				width = "100%",
+				height = "100%",
+				opacity = 0.95,
+			},
+		}
+	end
+
+	if name == "ZEN_MODE" then
+		local incremental = value:find("+")
+		local number_value = tonumber(value)
+		if incremental ~= nil then
+			while number_value > 0 do
+				window:perform_action(wezterm.action.IncreaseFontSize, pane)
+				number_value = number_value - 1
+			end
+		elseif number_value < 0 then
+			window:perform_action(wezterm.action.ResetFontSize, pane)
+			overrides.font_size = nil
+		else
+			overrides.font_size = number_value
+		end
+	end
+	if name == "DIFF_VIEW" then
+		local incremental = value:find("+")
+		local number_value = tonumber(value)
+		if incremental ~= nil then
+			while number_value > 0 do
+				window:perform_action(wezterm.action.DecreaseFontSize, pane)
+				number_value = number_value - 1
+			end
+			-- overrides.background = {
+			-- 	w.set_nvim_wallpaper("Diffview.jpeg"),
+			--
+			-- 	{
+			-- 		source = {
+			-- 			Gradient = {
+			-- 				colors = { "#000000" },
+			-- 			},
+			-- 		},
+			-- 		width = "100%",
+			-- 		height = "100%",
+			-- 		opacity = 0.95,
+			-- 	},
+			-- }
+		elseif number_value < 0 then
+			window:perform_action(wezterm.action.ResetFontSize, pane)
+			overrides.background = nil
+			overrides.font_size = nil
+		else
+			overrides.font_size = number_value
+		end
+	end
+	window:set_config_overrides(overrides)
+end)
+
+return config
